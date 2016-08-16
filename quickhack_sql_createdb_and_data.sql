51e8da451c73952cdb0cec3641a561a78018a256
Ridwan <r.sarwari@vigilvideo.com>
2016-08-15 22:08:32 -0700

    Created Table Students  and sample data

--- quickhack_sql_createdb_and_data.sql
+++ quickhack_sql_createdb_and_data.sql
@@ -11,14 +11,10 @@
 -- ---------------------------------------------------------------------------------
 
 
-
 DROP DATABASE IF EXISTS wemoconnect_bootcamp1;
 CREATE DATABASE wemoconnect_bootcamp1;
 
 USE wemoconnect_bootcamp1;
-
-
-
 
 DROP  TABLE IF EXISTS coaches;
 
@@ -32,5 +28,22 @@ INSERT INTO coaches VALUES (0, 'John Doe');
 INSERT INTO coaches VALUES (0, 'Bruce Wayn');
 INSERT INTO coaches VALUES (0, 'Jacky Chan');
 INSERT INTO coaches VALUES (0, 'Any other coach');
+
+
+DROP  TABLE IF EXISTS students;
+
+CREATE TABLE students (
+	id  int NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+	name varchar(100),
+	phone varchar(100),
+	email varchar(100)
+);
+
+INSERT INTO students VALUES (1, 'ali', '408something', 'alisomething@somecompany.com');
+INSERT INTO students VALUES (2, 'hamza', '408something', 'alisomething@somecompany.com');
+INSERT INTO students VALUES (3, 'ridwan', '408something', 'alisomething@somecompany.com');
+INSERT INTO students VALUES (4, 'obaid', '408something', 'alisomething@somecompany.com');
+
+
 
 
